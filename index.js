9cc0fb8489d4d67d081d19d6d65cd7f7d5023e87
Olayiwola Olanrewaju <larry_coal@outlook.com>
2022-12-22 14:28:32 -0500

    refactor

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ server.start().then(() => {
   server.applyMiddleware({ app, path: "/graphql", cors: true });
 });
 const app = express();
+app.use(
+  express.static(path.join(__dirname, "/client/build"))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/client/build", "index.html")
+  );
+});
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
 });
